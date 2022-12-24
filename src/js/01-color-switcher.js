415508d69c1632959ca3f7ba8c370bf0338f81c8
Viktoriia-Lehusova <vika01059953@gmail.com>
2022-12-24 13:20:25 +0200

    add 1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector('button[data-start]');
+console.log(btnStart);
+const btnStop = document.querySelector('button[data-stop]');
+console.log(btnStop);
+const body = document.querySelector('body');
+console.log(body);
+const INTERVAL_DELAY = 1000;
+let timeId = null;
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+btnStop.disabled = true;
+
+function onBtnStartClick(evt) {
+  const isActive = false;
+
+  timeId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor(body.style.backgroundColor);
+    console.log('change color');
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  }, INTERVAL_DELAY);
+}
+
+function onBtnStopClick(evt) {
+  clearInterval(timeId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
